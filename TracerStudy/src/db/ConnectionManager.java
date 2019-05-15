e041c3d5a4839a9bf53764f171841ff266dc0f93
Gilbert Takarasel <49542765+Gilberttakarasel@users.noreply.github.com>
2019-05-16 03:29:05 +0700

    ganti nama dataabase

--- TracerStudy/src/db/ConnectionManager.java
+++ TracerStudy/src/db/ConnectionManager.java
@@ -16,7 +16,7 @@ import java.sql.*;
 public class ConnectionManager {
     private Connection con;
     private String driver = "com.mysql.jdbc.Driver";
-    private String url = "jdbc:mysql://localhost:3306/pbd_akhir";
+    private String url = "jdbc:mysql://localhost:3306/tracerstudy";
     private String username = "root";
     private String password = "";
     
