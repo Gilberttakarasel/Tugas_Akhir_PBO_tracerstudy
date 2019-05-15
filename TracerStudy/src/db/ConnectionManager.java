78f3e067972a738b6a087f18087e0ca747118a66
Gilbert Takarasel <49542765+Gilberttakarasel@users.noreply.github.com>
2019-05-16 03:27:31 +0700

    Connecting

---
+++ TracerStudy/src/db/ConnectionManager.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db;
+
+import java.sql.*;
+
+
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ConnectionManager {
+    private Connection con;
+    private String driver = "com.mysql.jdbc.Driver";
+    private String url = "jdbc:mysql://localhost:3306/pbd_akhir";
+    private String username = "root";
+    private String password = "";
+    
+    public Connection logOn(){
+        try{
+            //Load JDBC Driver
+            Class.forName(driver).newInstance();
+            //Buat object connection
+            con = DriverManager.getConnection(url, username, password);
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return con;
+    }
+    
+    public void logOff(){
+        try{
+            //Tutup koneksi
+            con.close();
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+   
+    
+}
